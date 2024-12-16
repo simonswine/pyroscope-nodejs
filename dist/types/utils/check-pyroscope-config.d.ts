87434e2d4ba49bd639c9b792d9a2d6e0bd055653
Christian Simon <simon@swine.de>
2024-12-16 13:11:09 +0000

    WIP

---
+++ dist/types/utils/check-pyroscope-config.d.ts
@@ -0,0 +1,4 @@
+import { PyroscopeConfig } from '../pyroscope-config';
+export declare function checkPyroscopeConfig(
+  config: unknown
+): asserts config is PyroscopeConfig;
---
+++ dist/types/utils/process-config.d.ts
@@ -0,0 +1,6 @@
+import { Environment } from '../environment';
+import { PyroscopeConfig } from '../pyroscope-config';
+export declare function processConfig(
+  config: PyroscopeConfig,
+  env: Environment
+): PyroscopeConfig;
